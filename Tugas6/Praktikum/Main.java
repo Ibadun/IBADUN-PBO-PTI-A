0c36efed8f943b44b3d74af7bb73cd2f374d3ea6
Ibadun <160575611+Ibadun@users.noreply.github.com>
2024-05-01 16:49:39 +0700

    Main.java

--- Tugas6/Praktikum/Main.java
+++ Tugas6/Praktikum/Main.java
@@ -3,19 +3,19 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        Manusia manusia = new Manusia("John Doe", true, "1234567890", true);
+        Manusia manusia = new Manusia("Diana Puspa", true, "1234567890", true);
         System.out.println("Data Manusia:");
         System.out.println(manusia);
 
-        MahasiswaFILKOM mahasiswa = new MahasiswaFILKOM("Jane Smith", false, "0987654321", false, "1711502000", 3.75);
+        MahasiswaFILKOM mahasiswa = new MahasiswaFILKOM("Bunga Lestari", false, "0987654321", false, "1711502000", 3.75);
         System.out.println("\nData Mahasiswa FILKOM:");
         System.out.println(mahasiswa);
 
-        Pekerja pekerja = new Pekerja("Alice Johnson", true, "2345678901", true, 50000, LocalDate.of(2015, 7, 15), 2);
+        Pekerja pekerja = new Pekerja("Amanda Wahyu", true, "2345678901", true, 50000, LocalDate.of(2015, 7, 15), 2);
         System.out.println("\nData Pekerja:");
         System.out.println(pekerja);
 
-        Manager manager = new Manager("Bob Brown", true, "3456789012", true, 80000, LocalDate.of(2010, 3, 20), 3, "IT");
+        Manager manager = new Manager("Zacky Fadilla", true, "3456789012", true, 80000, LocalDate.of(2010, 3, 20), 3, "IT");
         System.out.println("\nData Manager:");
         System.out.println(manager);
     }
